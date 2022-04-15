80472df58a1934d87d468a8908ed645563411ac2
sofienneee <101095315+sofienneee@users.noreply.github.com>
2022-04-15 01:35:52 +0100

    Update ennemi.h

--- sofienne/ennemi.h
+++ sofienne/ennemi.h
@@ -7,7 +7,7 @@
 
 typedef struct
 {SDL_Surface *enemr[12];
-SDL_Surface *enemrl[12];
+SDL_Surface *eneml[12];
 SDL_Surface *enemat[23];
 SDL_Rect posenemy;
 
@@ -16,10 +16,11 @@ SDL_Rect posenemy;
 
 void initEnnemi(Ennemi*e);
 void afficherEnnemi(Ennemi e, SDL_Surface * screen);
-void animerEnnemi( Ennemi * e);
-void deplacer( Ennemi * e);
+void animerEnnemi( Ennemi * e,SDL_Rect posp,SDL_Surface * screen);
+void deplacer( Ennemi * e,SDL_Surface* screen);
 int collisionBB( SDL_Rect posp, SDL_Rect pose);
 void deplacerIA( Ennemi * e, SDL_Rect posPerso);
+
 
 
 
